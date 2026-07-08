3a17df82b2a8e6828a462ebc60b80c054f1f3c3d
yz <yazki.maulana@gmail.com>
2026-07-08 22:51:51 +0700

    chore: initialize laravel application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Transport Booking') }}</title>
 
         @fonts
 
